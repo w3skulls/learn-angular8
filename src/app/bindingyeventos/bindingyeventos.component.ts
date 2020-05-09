59520eba1919a2cfe818c52bc01b23c5d6d2c0c1
rushrats <hectorochoa.bellon@gmail.com>
2020-05-09 19:29:50 +0200

    Creación componente para aprender a bindear

---
+++ src/app/bindingyeventos/bindingyeventos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bindingyeventos',
+  templateUrl: './bindingyeventos.component.html',
+  styleUrls: ['./bindingyeventos.component.scss']
+})
+export class BindingyeventosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
