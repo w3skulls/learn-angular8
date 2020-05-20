675b7c546ccb85491250ee28bca37c45690ed350
rushrats <hectorochoa.bellon@gmail.com>
2020-05-20 14:17:17 +0200

    directiva *ngFor

--- src/app/ng-for/ng-for.component.ts
+++ src/app/ng-for/ng-for.component.ts
@@ -13,14 +13,33 @@ export class NgForComponent implements OnInit {
 
   cursos = [
     {
+      id: 1,
       titulo: 'Curso de React de Carlos Azaustre',
       duracion: 21 
     },
     {
+      id: 2,
       titulo: 'Curso de Angular 8 - Desde básico hasta nivel profesional',
       duracion: 5
     }
   ];
+
+  onActualizar(ev){
+    console.log(TAG, "Actualizando json de cursos");
+    let nuevoCurso = JSON.parse('{"titulo":"Nodejs de 0 a experto","duracion":100}');
+    this.cursos.push(nuevoCurso);
+  }
+
+  onBorrar(curso){
+    console.log(TAG, "Borrando curso: " + curso.id + " que se encuentra en la posición " + this.cursos.indexOf(curso) + " del array de cursos.");
+    this.cursos.splice(this.cursos.indexOf(curso), 1);
+
+  }
+
+  onEditar(curso){
+    console.log(TAG, "Editando curso");
+    this.cursos[this.cursos.indexOf(curso)].titulo = "No disponible";
+  }
 
   ngOnInit(): void {
     console.log(TAG, "Iniciando");
